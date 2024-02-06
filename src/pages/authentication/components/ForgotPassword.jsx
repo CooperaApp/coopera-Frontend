0c9737720a274357bec4c0ba984b84707d0adfc3
Sgreen <Sgreensneh@gmail.com>
2024-02-06 23:41:56 +0100

    dashboard statistics

--- src/pages/authentication/components/ForgotPassword.jsx
+++ src/pages/authentication/components/ForgotPassword.jsx
@@ -2,14 +2,12 @@ import { useState } from "react";
 import ArrowBack from "../../../assets/images/png/arrow-back.png";
 import CooperaLogo from "../../../assets/images/svg/CooperaLogo.svg";
 import DashboardImage from "../../../assets/images/svg/DashboardImg2.svg";
-import BackIcon from  "../../../assets/images/svg/Back-Icon.svg";
+import BackIcon from "../../../assets/images/svg/Back-Icon.svg";
 import { ForgetPassword } from "../../../utils/api/CooperativeAPICalls";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 
-
 const ForgotPassword = () => {
-  
   const navigate = useNavigate();
 
   let initialState = {
@@ -18,7 +16,7 @@ const ForgotPassword = () => {
 
   const [data, setData] = useState(initialState);
 
-  function handleChange (event) {
+  function handleChange(event) {
     setData({
       ...data,
       [event.target.name]: event.target.value,
@@ -28,12 +26,12 @@ const ForgotPassword = () => {
   const forgotPassword = async () => {
     try {
       const res = ForgetPassword(initialState.email);
-    toast.success("Email sent, please check you mail");
-    navigate("/reset-password")
+      toast.success("Email sent, please check you mail");
+      navigate("/reset-password");
     } catch (error) {
-    toast.error("reset.password.failed");
+      toast.error("reset.password.failed");
     }
-  }
+  };
 
   return (
     <div className="flex h-screen overflow-y-hidden pt-0">
@@ -53,8 +51,13 @@ const ForgotPassword = () => {
           />
         </div>
         <div className="h-40 w-96 mt-12 ml-28 ">
-          <p className="mb-5 authentication-big-font-style">Build your Cooperative Society using Coopera</p>
-          <p className="authentication-small-font-style">With Coopera, managing your cooperative society is seamless. Elevate efficiency and foster financial growth</p>
+          <p className="mb-5 authentication-big-font-style">
+            Build your Cooperative Society using Coopera
+          </p>
+          <p className="authentication-small-font-style">
+            With Coopera, managing your cooperative society is seamless. Elevate
+            efficiency and foster financial growth
+          </p>
         </div>
 
         <div className="mt-10 transition-all duration-300 rounded-lg blur-sm hover:blur-none ">
@@ -62,7 +65,7 @@ const ForgotPassword = () => {
             className="w-96 ml-28 -m-32"
             src={DashboardImage}
             alt="Your Image"
-            style={{ height: "705px", width: "489px"}}
+            style={{ height: "705px", width: "489px" }}
           />
         </div>
       </div>
@@ -70,16 +73,17 @@ const ForgotPassword = () => {
       <div className="w-1/2 p-10 mt-20">
         <img src={CooperaLogo} alt="Logo" className="h-10 w-10 -mt-6" />
         <h2 className="forgotten-password-big-font">Forgotten Password?</h2>
-        <p className='forgotten-password-small-font'>No worries! We will send reset instructions to you</p>
+        <p className="forgotten-password-small-font">
+          No worries! We will send reset instructions to you
+        </p>
         <form className="" onSubmit={forgotPassword}>
-
           <div className="mb-5">
             <label className="sub-text-font-style">Cooperative Email</label>
             <br />
             <input
               type="email"
               className="w-full h-10 px-4 text-xs"
-              style={{backgroundColor: "#F3F3F3", borderRadius: "4px"}}
+              style={{ backgroundColor: "#F3F3F3", borderRadius: "4px" }}
               placeholder="Cooperative Email"
               onChange={handleChange}
               value={data.email}
@@ -88,13 +92,18 @@ const ForgotPassword = () => {
           </div>
 
           <div className="w-full h-10 px-4 rounded-md mb-2 bg-[#7C39DE] mt-12 cursor-pointer border-2 border-[#7C39DE] text-white flex items-center justify-center font-bold">
-            <button className="font-semibold font-20 font-sans " type="submit">Reset Password</button>
+            <button className="font-semibold font-20 font-sans " type="submit">
+              Reset Password
+            </button>
           </div>
         </form>
 
         <div className="flex shrink-0 items-center justify-center">
-          <a className="text-sm text-[#7C39DE] flex font-sans whitespace-break-spaces font-bold" href="/login">
-            <img src={BackIcon}/> Back to Login
+          <a
+            className="text-sm text-[#7C39DE] flex font-sans whitespace-break-spaces font-bold"
+            href="/login"
+          >
+            <img src={BackIcon} /> Back to Login
           </a>
         </div>
       </div>
