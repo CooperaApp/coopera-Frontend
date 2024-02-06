d74cbb0a0778438049e86bcfe869384d803a6bf6
James <jamesasuelimen77@gmail.com>
2024-02-06 13:33:55 +0100

    dev pull

--- src/pages/authentication/components/Login.jsx
+++ src/pages/authentication/components/Login.jsx
@@ -7,6 +7,7 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import {  toast } from "react-toastify";
 
+
 const Login = () => {
   const navigate = useNavigate();
   const [formData, setFormData] = useState({
